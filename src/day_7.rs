5cb73493caeb4a1fc7995293286c1cb99173f88b
Hampus Gunnarsson <takeoutguhui@gmail.com>
2020-12-17 09:08:49 +0100

    clippy + fmt

--- src/day_7.rs
+++ src/day_7.rs
@@ -9,19 +9,23 @@ fn parse_line<'a>(h_map: &mut HashMap<&'a str, Vec<&'a str>>, line: &'a str, re:
     let mut cap_iter = re.captures_iter(line);
     let color = cap_iter.next().unwrap().get(1).unwrap().as_str();
     for cap in cap_iter {
-        bags.push(cap.get(1).unwrap().as_str());   
+        bags.push(cap.get(1).unwrap().as_str());
     }
     h_map.insert(color, bags);
 }
 
-pub fn find_gold(color: &str, h_map: &HashMap<&str, Vec<&str>>) -> bool{
+pub fn find_gold(color: &str, h_map: &HashMap<&str, Vec<&str>>) -> bool {
     if color == "shiny gold" {
-        return true;
-    } else if color == "no other"{
-        return false
+        true
+    } else if color == "no other" {
+        false
     } else {
-        h_map.get(color).unwrap().iter().any(|clr| find_gold(clr, h_map))
-    } 
+        h_map
+            .get(color)
+            .unwrap()
+            .iter()
+            .any(|clr| find_gold(clr, h_map))
+    }
 }
 
 pub fn part_1() {
@@ -30,9 +34,9 @@ pub fn part_1() {
     let lines = read_lines_true("input/day_7.txt").unwrap();
 
     for line in &lines {
-        parse_line(&mut h_map, line, &re);    
+        parse_line(&mut h_map, line, &re);
     }
-   
+
     let result = h_map.keys().filter(|key| find_gold(key, &h_map)).count();
-    println!("The result is {}", result-1);
+    println!("The result is {}", result - 1);
 }
